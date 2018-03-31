b97a8822a57a609b4fab9c6b92fd65b1150456e6
Muhammed Athimannil <hello@athimannil.com>
2018-04-01 01:45:04 +0200

    home page redirect to /questions/

--- client/components/questions/Questions.js
+++ client/components/questions/Questions.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 class Questions extends React.Component {
   render() {
+    console.log(this.props);
     return (
       <section className="container">
         <h1>Hello mate</h1>
