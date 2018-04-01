8a56e91befb73eabf6fa29bb08b0e99328a52c60
Muhammed Athimannil <hello@athimannil.com>
2018-04-01 04:10:52 +0200

    update on input text

--- client/components/common/TextInput.js
+++ client/components/common/TextInput.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const TextInput = ({name, label, placeholder}) => {
   return (
     <div className="form-wrapper">
       <h2>{label}</h2>
-      <div className="field">
-        <input
-          type="text"
-          className="form-wrapper-text"
-          name={name}
-          placeholder={placeholder}
-        />
-      </div>
+      <input
+        type="text"
+        className="form-wrapper-text"
+        name={name}
+        placeholder={placeholder}
+      />
     </div>
   );
 };
