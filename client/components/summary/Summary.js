244253cbfebeaaca7487f815547342995428c08c
Muhammed Athimannil <hello@athimannil.com>
2018-04-01 16:12:17 +0200

    list result in summary

--- client/components/summary/Summary.js
+++ client/components/summary/Summary.js
@@ -24,12 +24,28 @@ class Summary extends React.Component {
         const qObj = question.values.find(q => q.id === value);
         return qObj ? qObj.value : value;
       });
-      return aValues;
+
+      return {
+        title: question.title,
+        value: aValues.join(', ')
+      };
     });
 console.log(results);
     return(
       <section className="container">
         <h1>Summary</h1>
+        <ul className="summary">
+          {
+            results.map((result, i) => {
+              return (
+                <li key={i}>
+                  <h3 className="summary-question">{result.title}</h3>
+                  <p>{result.value}</p>
+                </li>
+              );
+            })
+          }
+        </ul>
         <div className="form-footer">
           <Link
             to={ `/questions/${lastQstId}`}
