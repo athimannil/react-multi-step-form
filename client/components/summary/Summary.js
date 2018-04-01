5809e9edad088e1534de6483fc5ee921e0ae0d97
Muhammed Athimannil <hello@athimannil.com>
2018-04-01 15:45:04 +0200

    return answer value

--- client/components/summary/Summary.js
+++ client/components/summary/Summary.js
@@ -14,12 +14,16 @@ class Summary extends React.Component {
     const lastQstId = questions[questions.length-1].questionID;
     const results = answers.map(answer => {
       const question = questions.find(q => q.questionID == answer.questionID);
-      console.log(question);
+      // console.log(question);
       let aValues =
         answer.answerValues instanceof Array
           ? answer.answerValues
           : [answer.answerValues];
 
+      aValues = aValues.map(value => {
+        const qObj = question.values.find(q => q.id === value);
+        return qObj ? qObj.value : value;
+      });
       return aValues;
     });
 console.log(results);
