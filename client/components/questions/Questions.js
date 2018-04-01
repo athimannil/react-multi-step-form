631b15d121c329bc02dc1b93f0a223efc8dea823
Muhammed Athimannil <hello@athimannil.com>
2018-04-01 02:12:54 +0200

    check param exist or not

--- client/components/questions/Questions.js
+++ client/components/questions/Questions.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Questions extends React.Component {
   render() {
-    console.log(this.props);
+    const { questions } = this.props;
+    console.log(questions);
+    console.log(this.props.match.params);
+    console.log(this.props.match.params.questionId);
+
+    let questionId;
+    if (this.props.match.params != undefined) {
+      questionId = this.props.match.params.questionId;
+    }
+    console.log('Question ID from param ', questionId);
+
     return (
       <section className="container">
-        <h1>Hello mate</h1>
+        {
+          questionId ?
+            <div>
+              <h1>Your question is here</h1>
+            </div> :
+            <div>
+              <h1>Start your survey</h1>
+              <div className="form-footer">
+                <Link to='/'>Home</Link>
+                <Link to='/questions/1'>Start</Link>
+              </div>
+            </div>
+        }
+
       </section>
     );
   }
 }
 
-export default Questions;
+export default Questions
