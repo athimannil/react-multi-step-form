f644cdfd9cc7af4b28bcce9e8165ebb3df0fb8dd
Muhammed Athimannil <hello@athimannil.com>
2018-04-01 02:45:14 +0200

    prev question footer link

--- client/components/questions/Questions.js
+++ client/components/questions/Questions.js
@@ -4,19 +4,18 @@ import { Link } from 'react-router-dom';
 class Questions extends React.Component {
   render() {
     const { questions } = this.props;
-    console.log(questions);
-    console.log(this.props.match.params);
-    console.log(this.props.match.params.questionId);
 
     const currentQuestion = questions.find(q => q.questionID ==  this.props.match.params.questionId);
-    console.log(currentQuestion);
     const currentQstIndex = questions.indexOf(currentQuestion);
-    console.log(currentQstIndex);
-    let nextQstId;
+
+    let prevQstId, nextQstId;
+    if(questions[currentQstIndex-1]){
+      prevQstId = questions[currentQstIndex-1].questionID;
+    }
+
     if(questions[currentQstIndex+1]){
       nextQstId = questions[currentQstIndex+1].questionID;
     }
-    console.log('nextQstId ', nextQstId);
 
     return (
       <section className="container">
@@ -31,7 +30,7 @@ class Questions extends React.Component {
         }
 
         <div className="form-footer">
-          <Link to='/' className="btn btn-secondary">Home</Link>
+          <Link to={ prevQstId ? `/questions/${prevQstId}` : '/questions/'} className="btn btn-secondary">Previous</Link>
           <Link to={`/questions/${nextQstId}`} className="btn btn-parimary">Next</Link>
         </div>
       </section>
