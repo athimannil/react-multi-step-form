5cec8166926d251595f383dfb8ca09fd8d073ae2
Muhammed Athimannil <hello@athimannil.com>
2018-04-01 15:31:46 +0200

    back button to last question

--- client/components/summary/Summary.js
+++ client/components/summary/Summary.js
@@ -1,14 +1,36 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Summary extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render () {
+    console.log(this.props);
+    const { questions, answers } = this.props;
+    console.log(questions, answers);
+    const lastQstId= questions[questions.length-1].questionID;
     return(
       <section className="container">
         <h1>Summary</h1>
+        <div className="form-footer">
+          <Link
+            to={ `/questions/${lastQstId}`}
+            className="btn btn-secondary"
+          >Back</Link>
+        </div>
       </section>
     );
   }
 }
 
-export default Summary;
+const mapStateToProps = (state) => {
+  return {
+    questions: state.questions,
+    answers: state.answers
+  };
+};
+
+export default connect(mapStateToProps, null)(Summary);
