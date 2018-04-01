082087d3c1ec65ccdd442dc6c27278ee13a827e6
Muhammed Athimannil <hello@athimannil.com>
2018-04-01 15:21:54 +0200

    router path to summary page

---
+++ client/components/summary/Summary.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Summary extends React.Component {
+  render () {
+    return(
+      <section className="container">
+        <h1>Summary</h1>
+      </section>
+    );
+  }
+}
+
+export default Summary;
