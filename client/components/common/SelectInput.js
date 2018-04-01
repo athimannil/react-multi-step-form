a87fa94d6cb694717e597df4f84acf0cbc9dae8b
Muhammed Athimannil <hello@athimannil.com>
2018-04-01 04:02:16 +0200

    dropwdown style

--- client/components/common/SelectInput.js
+++ client/components/common/SelectInput.js
@@ -4,17 +4,15 @@ const SelectInput = ({name, label, options, defaultOption}) => {
   return (
     <div className="form-wrapper">
       <h2>{label}</h2>
-      <div className="field">
-        <select
-          name={name}
-          className="form-control"
-        >
-          <option value="">{defaultOption}</option>
-          {options.map((option) => {
-            return <option key={option.id} value={option.id} >{option.value}</option>;
-          })}
-        </select>
-      </div>
+      <select
+        name={name}
+        className="form-wrapper-select"
+      >
+        <option value="">{defaultOption}</option>
+        {options.map((option) => {
+          return <option key={option.id} value={option.id} >{option.value}</option>;
+        })}
+      </select>
     </div>
   );
 };
