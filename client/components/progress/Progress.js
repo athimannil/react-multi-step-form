3dcd03229c0f90f58eafeda805f39e9d6fe2dd38
Muhammed Athimannil <hello@athimannil.com>
2018-04-05 17:44:17 +0200

    remove console

--- client/components/progress/Progress.js
+++ client/components/progress/Progress.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 const Progress = ({length, currentIndex}) => {
-  console.clear();
-  console.log(length)
   return (
     <ul className="multi-steps">
       { [...Array(length)].map((i, index) => {
